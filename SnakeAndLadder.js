3da0915880dbf211ef0c956904083a6b8bec05f0
purushothaman-source <purushoth99jun@gmail.com>
2021-02-09 07:34:47 +0530

    ADDED : Code to get dieMove

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,8 +1,15 @@
 console.log("Welcome to Snake and Ladder Game ");
 
 class SnakeAndLadder {
+    constructor() { }
     main() {
         var player = 1;
         const INITIAL_POSITION = 1;
+        this.rollDie();
+    }
+    rollDie() {
+        console.log(Math.floor(Math.random() * 6) + 1);
     }
 }
+var obj1 = new SnakeAndLadder();
+obj1.main();
