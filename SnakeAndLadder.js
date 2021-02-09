b6f3f9fef50caa26b5c35bfc2cadf95a1d87fe2e
purushothaman-source <purushoth99jun@gmail.com>
2021-02-09 06:43:28 +0530

    ADDED : Code to initialise player position

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -1,1 +1,8 @@
-console.log("Welcome to Snake and Ladder Game ")
+console.log("Welcome to Snake and Ladder Game ");
+
+class SnakeAndLadder {
+    main() {
+        var player = 1;
+        const INITIAL_POSITION = 1;
+    }
+}
